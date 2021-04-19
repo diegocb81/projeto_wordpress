9bd91129577cfe03ae1d92ba2b1aa60cd5f10b54
diegocb81 <diegocb81@gmail.com>
2021-04-19 16:31:19 -0300

    Iniciando o projeto

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Projeto WordPress</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="img/logo.png" alt="Logotipo"></div><!--logo-->
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Contato</a></li>
+            </ul><!--menu-desktop-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </section><!--topo-->
+    
+</body>
+</html>
