f401fe2052b8393e4c95b4b6c4e01b7fd465db34
diegocb81 <diegocb81@gmail.com>
2021-04-19 17:26:59 -0300

    Menu-desktop e aplicando responsivo

--- index.php
+++ index.php
@@ -21,6 +21,17 @@
                 <li><a href="">Contato</a></li>
             </ul><!--menu-desktop-->
             <div class="clear"></div>
+            <br/>
+            <br/>
+            <div class="w50 time-descricao">
+                <h2>Melhore a comunicação com seu cliente e time.</h2>
+                <p>Consultoria especializada em startups, empresas, e principalmente, pessoas.</p>
+                <a href="https://github.com/diegocb81" target="_blank">Ver demonstração</a>
+            </div><!--w50-time-descricao-->
+            <div class="w50 time-imagem">
+                <img src="img/equipe.png" alt="Equipe">
+            </div><!--w50-time-imagem-->
+            <div class="clear"></div>
         </div><!--center-->
     </section><!--topo-->
     
