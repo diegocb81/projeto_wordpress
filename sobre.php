7f4f106dccfae8721f3656aae3cbc2dca9bf1db8
diegocb81 <diegocb81@gmail.com>
2021-04-23 19:37:03 -0300

    Classe sobre-imagens e footer no sobre.php

--- sobre.php
+++ sobre.php
@@ -36,16 +36,64 @@
 
     <section class="sobre-equipe">
         <div class="center">
-        <div class="w50 equipe-sobre-texto">
-            <h2>Uma equipe estratégica para um trabalho incrível.</h2>
-            <p>Com foco permanente na geração de resultados para empresas, atuamos na consultoria estratégica e na criação e implementação de soluções de comunicação criativas, consistentes e adequadas às necessidades e características de cada companhia. Para isso, trabalhamos sempre em parceria e colaboração com interfaces e executivos das organizações, mantendo completo envolvimento com os negócios de cada cliente.</p>
-            </div><!--center-->
-        </div><!--w50-->
+            <div class="w50 equipe-sobre-texto">
+                <h2>Uma equipe estratégica para um trabalho incrível.</h2>
+                <p>Com foco permanente na geração de resultados para empresas, atuamos na consultoria estratégica e na criação e implementação de soluções de comunicação criativas, consistentes e adequadas às necessidades e características de cada companhia. Para isso, trabalhamos sempre em parceria e colaboração com interfaces e executivos das organizações, mantendo completo envolvimento com os negócios de cada cliente.</p>
+            </div><!--w50-equipe-sobre-texto-->
         <div class="w50">
-            <img src="img/equipe.jpg" alt="">
+            <img src="img/equipe.jpg" alt="Equipe">
         </div><!--w50-->
         <div class="clear"></div>
+        </div><!--center-->
     </section><!--sobre-equipe-->
+
+    <section class="sobre-imagens">
+        <div class="center">
+            <div class="img-sobre">
+                <img src="img/imagem-sobre.jpg" alt="Neural">
+            </div><!--img-sobre-->
+            <div class="img-sobre">
+                <img src="img/imagem-sobre.jpg" alt="Neural">
+            </div><!--img-sobre-->
+            <div class="img-sobre">
+                <img src="img/imagem-sobre.jpg" alt="Neural">
+            </div><!--img-sobre-->
+            <div class="img-sobre">
+                <img src="img/imagem-sobre.jpg" alt="Neural">
+            </div><!--img-sobre-->
+        </div><!--center-->
+    </section><!--sobre-imagens-->
+
+    <footer>
+        <section class="metodologia">
+            <div class="center">
+                <h2>Conheça nossa metodologia</h2>
+                <p>O que acha de fazermos o que mais gostamos de fazer? Conversar!<br>Entre em contato por e-mail ou
+                    telefone</p>
+                <a href="/contato">Entrar em contato</a>
+            </div><!--center-->
+        </section><!--metodologia-->
+        <div class="center">
+            <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="">Contato</a>
+                <a href="">FAQ</a>
+            </div><!--col-footer-->
+            <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="">Contato</a>
+                <a href="">FAQ</a>
+            </div><!--col-footer-->
+            <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="">Contato</a>
+                <a href="">FAQ</a>
+            </div><!--col-footer-->
+            <div style="width: 40%;text-align:right;" class="col-footer">
+                <img src="img/logo-dummy.png" alt="Logo">
+            </div><!--col-footer-->
+        </div><!--center-->
+    </footer>
 
 </body>
 
