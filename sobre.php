ab2aa9cc4b97ba74250e948243f6b2d5fb39a3e9
diegocb81 <diegocb81@gmail.com>
2021-04-23 00:38:55 -0300

    Desenvolvendo sobre.php e responsivo

--- sobre.php
+++ sobre.php
@@ -34,6 +34,19 @@
         </div><!--center-->
     </section><!--topo-->
 
+    <section class="sobre-equipe">
+        <div class="center">
+        <div class="w50 equipe-sobre-texto">
+            <h2>Uma equipe estratégica para um trabalho incrível.</h2>
+            <p>Com foco permanente na geração de resultados para empresas, atuamos na consultoria estratégica e na criação e implementação de soluções de comunicação criativas, consistentes e adequadas às necessidades e características de cada companhia. Para isso, trabalhamos sempre em parceria e colaboração com interfaces e executivos das organizações, mantendo completo envolvimento com os negócios de cada cliente.</p>
+            </div><!--center-->
+        </div><!--w50-->
+        <div class="w50">
+            <img src="img/equipe.jpg" alt="">
+        </div><!--w50-->
+        <div class="clear"></div>
+    </section><!--sobre-equipe-->
+
 </body>
 
 </html>
