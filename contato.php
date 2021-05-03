0389d88d05f797c93a8c060fb72953e4ed19e827
diegocb81 <diegocb81@gmail.com>
2021-05-02 23:58:46 -0300

    Atualizando dados de contato

--- contato.php
+++ contato.php
@@ -39,7 +39,9 @@
     <section class="contato">
         <div class="center">
             <div class="w50">
-
+                <h2>A mais importante, <br/>a primeira conversa</h2>
+                <p><b>Telefone:</b> (48) 99947-7316</p>
+                <p><b>E-mail:</b> diegocb81@gmail.com</p>
             </div><!--w50-->
         </div><!--center-->
     </section><!--contato-->
