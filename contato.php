6f25a6b914cd1e4ff544441cb2152796d8e1a8bc
diegocb81 <diegocb81@gmail.com>
2021-05-03 00:51:12 -0300

    Desenvolvendo formulário de contato

--- contato.php
+++ contato.php
@@ -40,7 +40,6 @@
         <div class="center">
             <div class="w50 contato-info">
                 <h2>A mais importante, <br/>primeira conversa</h2>
-                <br/>
                 <p><b>Telefone:</b> (48) 99947-7316</p>
                 <p><b>E-mail:</b> diegocb81@gmail.com</p>
                 <p><b>Endereço:</b> Travessa Montevidéu, 98 - Cep: 88058-083 - Florianópolis/SC.</p>
@@ -48,6 +47,17 @@
                 <div id="mapa"></div><!--mapa-->
             </div><!--mapa-container-->
             </div><!--w50-contato-info-->
+            <div class="w50 contato-form">
+                <form>
+                    <input type="text">
+                    <input type="text">
+                    <input type="text">
+                    <input type="text">
+                    <select></select>
+                    <textarea></textarea>
+                    <input type="submit" value="Enviar!">
+                </form>
+            </div><!--w50-contato-form-->
             <div class="clear"></div>
         </div><!--center-->
     </section><!--contato-->
