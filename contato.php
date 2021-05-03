cf75360dfdfdc227ba8f219601b2068938a56fc7
diegocb81 <diegocb81@gmail.com>
2021-05-03 00:28:28 -0300

    Iniciando e estilizando a classe mapa-container

--- contato.php
+++ contato.php
@@ -38,11 +38,17 @@
 
     <section class="contato">
         <div class="center">
-            <div class="w50">
-                <h2>A mais importante, <br/>a primeira conversa</h2>
+            <div class="w50 contato-info">
+                <h2>A mais importante, <br/>primeira conversa</h2>
+                <br/>
                 <p><b>Telefone:</b> (48) 99947-7316</p>
                 <p><b>E-mail:</b> diegocb81@gmail.com</p>
-            </div><!--w50-->
+                <p><b>Endereço:</b> Travessa Montevidéu, 98 - Cep: 88058-083 - Florianópolis/SC.</p>
+            <div class="mapa-container">
+                <div id="mapa"></div><!--mapa-->
+            </div><!--mapa-container-->
+            </div><!--w50-contato-info-->
+            <div class="clear"></div>
         </div><!--center-->
     </section><!--contato-->
 
