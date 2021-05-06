713c3e5e9be281a1633c04f3530549bba1389b60
Diego Barcellos <diegocb81@gmail.com>
2021-05-06 15:40:32 -0300

    Inserindo ícone na aba do site

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" href="img/icone.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/all.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
